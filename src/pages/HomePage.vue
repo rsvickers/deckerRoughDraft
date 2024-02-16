7dffbc20aff99afc50c4d55efb45f0c38d0ffc81
Rhys Vickers <141367198+rsvickers@users.noreply.github.com>
2024-02-15 22:57:49 -0700

    initialize

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,35 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.home {
+  display: grid;
+  height: 80vh;
+  place-content: center;
+  text-align: center;
+  user-select: none;
+
+  .home-card {
+    width: clamp(500px, 50vw, 100%);
+
+    >img {
+      height: 200px;
+      max-width: 200px;
+      width: 100%;
+      object-fit: contain;
+      object-position: center;
+    }
+  }
+}
+</style>
